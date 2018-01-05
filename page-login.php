27ebd4f829faf3164a33a8b2b961b309c8849e08
JMMgit <jm.muan@gmail.com>
2018-01-05 20:58:57 +0800

    update

--- page-login.php
+++ page-login.php
@@ -4,41 +4,41 @@
  */
 get_header();
 ?>
-<main>
-<section class="container border rounded p-5 my-5">
-    <div class="row justify-content-center align-items-stretch">
-        <section class="col-md-6 col-sm-12 d-flex flex-column align-items-center rounded text-center p-4">
-                <section class="w-75" >
-                        <input class="form-control my-2" type="text" placeholder="Full Name" required>
-                        <input class="form-control my-2" type="email" placeholder="Email" required>
-                        <input class="form-control my-2"  type="password" placeholder="Password" required>
-                        <input class="form-control my-2" type="password" placeholder="Repeat password" required>
-                </section>
-                <section class="w-75">
-                        <label for="card d-inline-block">
-                            Card type:
-                        </label>
-                        <select class="form-control w-50 d-inline"  id="card" name="usercard">
-                            <option value="visa">Visa</option>
-                            <option value="mc">Mastercard</option>
-                            <option value="amex">American Express</option>
-                        </select>
-                        <input class="form-control my-2" type="text" placeholder="Card Number">
-                        <input class="form-control my-2" type="text" placeholder="Expiration Date(mm/yy)">
-                </section>
-                <input class="submit my-2 btn btn-primary w-50 mx-auto" type="submit" value="Register">
-        </section>
-        <section class="col-md-4 col-sm-12 p-4 d-flex flex-column justify-content-center">
+<main class="w-100 border bg-light">
+<section class="container p-5 my-5">
+    <div class="row justify-content-center">
+        <section class="border rounded-left bg-white col-md-6 col-sm-12 d-flex flex-column align-items-center rounded text-center py-5">
+            <form action="">
+                    <input class="form-control my-2" type="text" placeholder="Full Name" required>
                     <input class="form-control my-2" type="email" placeholder="Email" required>
                     <input class="form-control my-2"  type="password" placeholder="Password" required>
-                    <div class="form-check text-center">
-                        <input type="checkbox" class="form-check-input" id="remember">
-                        <label class="form-check-label pl-0" for="remember">Remember me</label>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-around">
-                        <a  href="#">Forgot Password?</a>
-                        <input class="submit my-2 btn btn-primary border w-25" type="submit" value="Login">
-                    </div>
+                    <input class="form-control my-2" type="password" placeholder="Repeat password" required>
+                    <label for="card d-inline-block">
+                        Card type:
+                    </label>
+                    <select class="form-control w-50 d-inline"  id="card" name="usercard">
+                        <option value="visa">Visa</option>
+                        <option value="mc">Mastercard</option>
+                        <option value="amex">American Express</option>
+                    </select>
+                    <input class="form-control my-2" type="text" placeholder="Card Number">
+                    <input class="form-control my-2" type="text" placeholder="Expiration Date(mm/yy)">
+            <input class="submit my-2 btn btn-primary w-50 mx-auto" type="submit" value="Register">
+            </form>
+        </section>
+        <section class="border rounded-right bg-white col-md-4 col-sm-12 p-4 d-flex flex-column justify-content-center align-items-strech">
+            <form action="">
+                <input class="form-control my-3" type="email" placeholder="Email" required>
+                <input class="form-control my-3"  type="password" placeholder="Password" required>
+                <div class="form-check text-center">
+                    <input type="checkbox" class="form-check-input" id="remember">
+                    <label class="form-check-label pl-0" for="remember">Remember me</label>
+                </div>
+                <div class="d-flex align-items-center justify-content-around">
+                    <a  href="#">Forgot Password?</a>
+                    <input class="submit my-2 btn btn-primary border w-25" type="submit" value="Login">
+                </div>
+            </form>
         </section>
         </section>
     </div>
